debcef8ef7a5d4872cd51ddd02128016b0638b78
AbdukhalilovIslom <abdukhalilovislom2021@gmail.com>
2024-06-09 17:46:49 +0500

    feat

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Categories from "../../components/common/categories/categories";
 import Hero from "../../components/home/hero/hero";
 import categories from "../../data/categories.json";
-import Info from "./info/info";
-import Testimonial from "./testimonial/testimonial";
+import Info from "../../components/home/info/info";
+import Testimonial from "../../components/home/testimonial/testimonial";
 
 export default function Home() {
   return (
@@ -15,8 +15,8 @@ export default function Home() {
         data={categories.slice(0, 8)}
         title={"Categories"}
       />
-      <Info/>
-      <Testimonial/>
+      <Info />
+      <Testimonial />
     </>
   );
 }
